b7be87626824f6d08d1146737922cde1244f4d2e
Giovanni Armane <giovanniarmane@gmail.com>
2024-11-02 21:21:10 -0300

    feat(mini-redis): implement event loop request handling

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,48 @@
-#![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
-    net::{Shutdown, TcpListener, TcpStream},
+use std::net::SocketAddr;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) {
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:6379";
+    let addr = addr.parse::<SocketAddr>().expect("Not a valid IP address");
+
+    let listener = TcpListener::bind(addr)
+        .await
+        .expect(format!("TCP port {} should be available", addr.port()).as_str());
+
     loop {
-        stream
-            .read(&mut [0; 128])
-            .map(|_| stream.write_all(b"+PONG\r\n"))
-            .map_err(|err| {
-                println!("error: {:?}", err);
-                stream.shutdown(Shutdown::Both)
-            })
-            .expect("Connection failed to drop")
-            .ok();
+        listener
+            .accept()
+            .await
+            .map(|(stream, _)| tokio::spawn(handle_connection(stream)))
+            .map_err(|err| eprintln!("error: {:?}", err))
+            .ok()
+            .take();
     }
 }
 
-fn main() {
-    let addr = "127.0.0.1:6379";
-
-    TcpListener::bind(addr)
-        .expect("TCP port should be available")
-        .incoming()
-        .for_each(|stream| match stream {
-            Ok(stream) => handle_connection(stream),
-            Err(e) => println!("error: {}", e),
-        });
+async fn handle_connection(mut stream: TcpStream) {
+    loop {
+        match stream.read(&mut [0; 128]).await {
+            // Connection closed
+            Ok(0) => break,
+            // Request received.
+            Ok(_) => stream
+                .write_all(b"+PONG\r\n")
+                .await
+                .expect("response should've been sent"),
+            // Error while reading.
+            Err(err) => {
+                println!("error: {:?}", err);
+                stream
+                    .shutdown()
+                    .await
+                    .expect("connection should've shutdown gracefully");
+                break;
+            }
+        }
+    }
 }
